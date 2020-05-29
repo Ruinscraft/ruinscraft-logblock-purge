31ffe30c9e66d801e31f088456ae911ee930ad82
Dexton Anderson <contact@dextonanderson.com>
2020-05-29 10:05:23 -0500

    more implementation

--- src/main/java/com/ruinscraft/logblockpurge/LogblockPurgePlugin.java
+++ src/main/java/com/ruinscraft/logblockpurge/LogblockPurgePlugin.java
@@ -1,13 +1,18 @@
 package com.ruinscraft.logblockpurge;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class LogblockPurgePlugin extends JavaPlugin {
 
@@ -15,11 +20,11 @@ public class LogblockPurgePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        String host = getConfig().getString("");
-        int port = getConfig().getInt("");
-        String db = getConfig().getString("");
-        String user = getConfig().getString("");
-        String pass = getConfig().getString("");
+        String host = getConfig().getString("logblock-mysql-storage.host");
+        int port = getConfig().getInt("logblock-mysql-storage.port");
+        String db = getConfig().getString("logblock-mysql-storage.database");
+        String user = getConfig().getString("logblock-mysql-storage.username");
+        String pass = getConfig().getString("logblock-mysql-storage.password");
 
         PurgeRunnable runnable = new PurgeRunnable(host, port, db, user, pass);
 
@@ -54,21 +59,35 @@ public class LogblockPurgePlugin extends JavaPlugin {
         @Override
         public void run() {
             try {
-                connection = DriverManager.getConnection("");
+                connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + db, user, pass);
             } catch (SQLException e) {
                 e.printStackTrace();
-                return; // Could not make mysql connection, just return
+                return; // could not make mysql connection, just return
             }
 
             while (true) {
+                if (isCancelled()) {
+                    try {
+                        closeConnection(connection);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                    return; // bukkittask was canceled
+                }
+
                 try {
                     testConnection(connection);
                 } catch (Exception e) {
                     e.printStackTrace();
-                    return;
+                    return; // connection has been closed
                 }
 
-
+                try {
+                    purgeTables(connection);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
@@ -77,6 +96,26 @@ public class LogblockPurgePlugin extends JavaPlugin {
     private static void testConnection(Connection connection) throws Exception {
         if (connection == null || connection.isClosed()) {
             throw new Exception("jdbc connection failed");
+        }
+    }
+
+    private static void closeConnection(Connection connection) throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
+    private static void purgeTables(Connection connection) throws SQLException {
+        List<String> worlds = Bukkit.getWorlds().stream().map(World::getName).collect(Collectors.toList());
+
+        for (String world : worlds) {
+            try (PreparedStatement deleteBlocks = connection.prepareStatement("DELETE FROM `lb-" + world + "-blocks` WHERE date < limit 1000000;")) {
+                deleteBlocks.execute();
+            }
+
+            try (PreparedStatement deleteEntities = connection.prepareStatement("DELETE FROM `lb-" + world + "-entities` WHERE date < limit 1000000;")) {
+                deleteEntities.execute();
+            }
         }
     }
 
